10d6112c28a37041ff4da0563a70372214b8489d
Morgan Harmon <morganharmon@gmail.com>
2022-05-02 16:36:08 -0700

    Create ingredients form

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
+const form = document.getElementById('form');
 
 // let state
+let recipeArr = [];
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formD = new FormData(form);
+    const ingredient = formD.get('ingredient');
+    const quantity = formD.get('quantity');
+    const measurement = formD.get('measurement');
+    const recipe = { ingredient, quantity, measurement };
+    recipeArr.push(recipe);
+
+});
+
